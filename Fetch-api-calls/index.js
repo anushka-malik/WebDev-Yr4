5fdb720c19c219750a38c552fe087911aa8242e3
anushka-malik <am8819356@gmail.com>
2025-07-09 13:27:44 +0530

    event capture and bubbling , event delegation

--- Fetch-api-calls/index.js
+++ Fetch-api-calls/index.js
@@ -1,29 +1,38 @@
-let div= document.getElementById("para");
+let btn = document.getElementById("but");
+let container = document.getElementById("card-container");
+let div = document.getElementById("para");
 
-let btn= document.getElementById("but");
-btn.addEventListener("click",apiData);
+btn.addEventListener("click", apiData);
 
-async function apiData()
-{
-    let heading = document.createElement("h1");
-    heading.innerText="Loading data..."
-    div.appendChild(heading);
+async function apiData() {
+  // Show loading text
+  let heading = document.createElement("h1");
+  heading.innerText = "Loading data...";
+  div.appendChild(heading);
 
-    const response= await fetch("https://dummyjson.com/users");
-    const data = await response.json();
+  // Fetch data
+  const response = await fetch("https://dummyjson.com/products");
+  const data = await response.json();
 
-    div.removeChild(heading);
-    
-    const users=data.users;
+  // Remove loading text
+  div.removeChild(heading);
 
-    let list = document.createElement("ul");
-    users.map((item)=>{
-        let el= document.createElement("li");
-        el.innerText=item.firstName;
-        list.appendChild(el);
-    })
+  const products = data.products;
 
-    div.appendChild(list);
+  // Clear old cards if any
+  container.innerHTML = "";
+
+  // Create cards for each product
+  products.map((product) => {
+    let card = document.createElement("div");
+    card.className = "card";
+
+    card.innerHTML = `
+      <h2>${product.id}. ${product.title}</h2>
+      <p>${product.description}</p>
+    `;
+
+    container.appendChild(card);
+  });
 }
-
 
